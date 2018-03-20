eb5dd959eb81e8562600a2259d4e1c2c6fc46c87
[pat-hanbury] <[patrickhanbury1@gmail.com]>
2018-03-19 22:28:58 -0400

    Added John's Updates to maze.h and maze.cpp

--- maze.h
+++ maze.h
@@ -1,107 +1,55 @@
-// Sample solution for project #5
+//John Kang and Patrick Hanbury
+//Algorithms
+//Project5a
+
+#ifndef MAZE_H
+#define MAZE_H
 
 #include <iostream>
-#include <limits.h>
-#include <list>
 #include <fstream>
-#include <queue>
-#include <vector>
 #include <stack>
-
 #include "d_except.h"
 #include "d_matrix.h"
 
-#define LargeValue 99999999
-
-using namespace std;
-
+//class maze variable declarations. 
 class maze
 {
 public:
-   maze(ifstream &fin);
-   void print(int,int,int,int);
-   bool isLegal(int i, int j);
-   void mapMazeToGraph(Graph &g);
-   void printPath(Graph::vertex_descriptor end,
+	
+	//construcor that takes in a maze file
+	maze(ifstream &fin);
+	//functions that checks to see if node was visited to clear
+   	void clearVisited(Graph &g);
+   	//function to set the weights of the nodes
+   	void setNodeWeights(Graph &g, int w);
+   	//function to mark the cleared nodes
+   	void clearMarked(Graph &g);
+   	//function to check if a move can be done. 
+	bool isLegal(int i, int j); 
+	
+	//function to print the mazees 
+   	void print(int,int,int,int); 
+	
+	//function that stores the maze in a graph to a graph
+   	void mapMazeToGraph(Graph &g);
+   	//function that prints the path
+   	void printPath(Graph::vertex_descriptor end,
                         stack<Graph::vertex_descriptor> &s,
-                        Graph g);
-   int numRows(){return rows;};
-   int numCols(){return cols;};
+                        Graph g); 
+                        
+   	int numRows(){return rows;};
+   	int numCols(){return cols;};
+
 
 private:
-   int rows; // number of rows in the maze
-   int cols; // number of columns in the maze12 a
-   
-   matrix<bool> value;
+	//number of rows
+	//number of columnms
+	int rows; 
+	int cols; 
+	//matrix to store which node is contained in each cell. 
+	matrix<Graph::vertex_descriptor> vertexCoords;
+	matrix<bool> value;   
 };
 
-maze::maze(ifstream &fin)
-// Initializes a maze by reading values from fin.  Assumes that the
-// number of rows and columns indicated in the file are correct.
-{
-   fin >> rows;
-   fin >> cols;
-   
-   char x;
-   
-   value.resize(rows,cols);
-   for (int i = 0; i <= rows-1; i++)
-      for (int j = 0; j <= cols-1; j++)
-      {
-         fin >> x;
-         if (x == 'O')
-            value[i][j] = true;
-         else
-            value[i][j] = false;
-      }
-   
-}
+#endif
 
-void maze::print(int goalI, int goalJ, int currI, int currJ)
-// Print out a maze, with the goal and current cells marked on the
-// board.
-{
-   cout << endl;
-
-   if (goalI < 0 || goalI > rows || goalJ < 0 || goalJ > cols)
-      throw rangeError("Bad value in maze::print");
-
-   if (currI < 0 || currI > rows || currJ < 0 || currJ > cols)
-      throw rangeError("Bad value in maze::print");
-
-   for (int i = 0; i <= rows-1; i++)
-   {
-      for (int j = 0; j <= cols-1; j++)
-      {
-	 if (i == goalI && j == goalJ)
-	    cout << "*";
-	 else
-	    if (i == currI && j == currJ)
-	       cout << "+";
-	    else
-	       if (value[i][j])
-		  cout << " ";
-	       else
-		  cout << "X";	  
-      }
-      cout << endl;
-   }
-   cout << endl;
-}
-
-bool maze::isLegal(int i, int j)
-// Return the value stored at the (i,j) entry in the maze, indicating
-// whether it is legal to go to cell (i,j).
-{
-   if (i < 0 || i > rows || j < 0 || j > cols)
-      throw rangeError("Bad value in maze::isLegal");
-
-   return value[i][j];
-}
-
-void maze::mapMazeToGraph(Graph &g)
-// Create a graph g that represents the legal moves in the maze m.
-
-void maze::printPath(Graph::vertex_descriptor end,
-                     stack<Graph::vertex_descriptor> &s,
-                     Graph g)
